83cfae42d3d47fde2ff7b8e20f276ed90094cbd7
Doyeon04 <rlaehdus0411@naver.com>
2022-12-16 08:53:25 +0900

    서버 프로젝트 세팅

---
+++ server/projectmate/src/test/java/com/toy/projectmate/ProjectmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.projectmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
