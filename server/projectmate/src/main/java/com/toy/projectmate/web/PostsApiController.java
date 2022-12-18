991d81206ff9f69877cd579425253a6215ac5637
Doyeon04 <rlaehdus0411@naver.com>
2022-12-18 11:32:44 +0900

    Swagger, Logback 설정 파일 추가

--- server/projectmate/src/main/java/com/toy/projectmate/web/PostsApiController.java
+++ server/projectmate/src/main/java/com/toy/projectmate/web/PostsApiController.java
@@ -2,7 +2,11 @@ package com.toy.projectmate.web;
 
 import com.toy.projectmate.service.posts.PostsService;
 import com.toy.projectmate.web.dto.PostsDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +16,26 @@ import org.springframework.web.bind.annotation.*;
 public class PostsApiController {
     private final PostsService postsService;
 
+    private final Logger logger = LoggerFactory.getLogger(PostsApiController.class);
+
+    @ApiOperation(value="게시물 등록")
     @PostMapping
     public ResponseEntity save(@RequestBody PostsDto.Request requestDto){
+        logger.info("save 메서드 호출");
         return ResponseEntity.ok(postsService.save(requestDto));
     }
-
+    @ApiOperation(value="게시물 수정")
     @PutMapping("/{id}")
     public ResponseEntity update(@PathVariable Long id, @RequestBody PostsDto.Request requestDto){
         return ResponseEntity.ok(postsService.update(id, requestDto));
     }
-
+    @ApiOperation(value="게시물 조회")
     @GetMapping("/{id}")
     public ResponseEntity read(@PathVariable Long id){
         return ResponseEntity.ok(postsService.findById(id));
     }
 
+    @ApiOperation(value="게시물 삭제")
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id){
         postsService.delete(id);
