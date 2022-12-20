f45362f769f0ba3df4476e6e7729b795dd450053
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2022-12-20 22:33:07 +0900

    router and details commit
    하위 버튼 라우터 설정

---
+++ client/projectmate/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style.css';
+const Header=()=>{
+    return(
+        <>
+            <div>
+                로고
+            </div>
+            <div><button className='login-btn'><span className="btn-text">로그인</span></button></div>
+            </>
+    );
+}
+
+export default Header;
