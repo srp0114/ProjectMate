85b05614a4a90db93746f4e41345603c2620ed87
Young <92440521+srp0114@users.noreply.github.com>
2022-12-21 02:07:40 +0900

    Update Details

--- client/projectmate/src/components/pages/Details.js
+++ client/projectmate/src/components/pages/Details.js
@@ -1,41 +1,59 @@
-import React, { useState }from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import data from '../data.js';
-import '../style/Shop.css'
+import '../App.css';
+import React from 'react';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Divider, Space, Typography, Input } from 'antd';
 
-const Card = () => {
-    const [cards, setCards] = useState(data);
+const { Title, Text } = Typography;
+const { TextArea } = Input;
 
-    return (
-        <>
-        <div className="Item">
-            {cards.map((card, i) => {
-                return (
-                    <Card className="Card">
-                    <CardMedia
-                        component="img"
-                        image={card.image}
-                    />
-                    <CardContent>
-                        <p>{card.title}</p>
-                        <p>{card.change}</p>
-                        <p>{card.address}</p>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Share</Button>
-                        <Button size="small">Learn More</Button>
-                    </CardActions>
-                    </Card>
+function App() {
+  return (
+    <div className="App">
+      <p className="postingDay">2022.12.18 20:43</p>
 
-                )
-            })}
+      <Title level={1} className="postingTitle">팀원 구합니다!</Title>
+
+      <br/>
+      <Space align="center">
+        <Avatar size={38} icon={<UserOutlined/>}/> 
+        <Text fontSize={100}>종강원해 (20)</Text> 
+      </Space>
+
+      <Divider/>
+
+      <div className="postingInfo">
+      <Space align="center" size={235}>
+        <Space align="center" size={100}> 
+        <Title level={4}>과목명</Title>
+        <Title level={4}>웹프레임워크1</Title>       
+        </Space>
+      
+        <Space align="center" size={135}>
+        <Title level={4}>분반</Title>
+        <Title level={4}>B반</Title>
+        </Space>
+      </Space>
+
+      <Space align="center" size={315}>
+        <Space align="center" size={85}> 
+        <Title level={4}>모집인원</Title>
+        <Title level={4}>4명</Title>
+        </Space>
+
+        <Space align="center" size={100}>
+        <Title level={4}>진행방식</Title>
+        <Title level={4}>오프라인</Title>
+        </Space>
+      </Space>
       </div>
-      </>
-    )
+      <Divider style={{ borderWidth: 5, borderColor: 'grey' }}  />
+      <br/>
+      <br/>
+      <TextArea readOnly={true} autoSize={{ minRows: 2, maxRows: 6 }} style={{resize: 'none', border:'none', fontSize:'18px'}}
+      value="팀플 주제는 아직 미정이지만, 쇼핑몰 프로젝트로 진행해볼까합니다. 
+      깃을 조금이라도 써보신 분이면 좋을 것 같고 연락 잘 되고, 열정적으로 참여하실 분 찾고 있습니다!"/>
+    </div>
+  );
 }
 
-export default Card;
+export default App;
