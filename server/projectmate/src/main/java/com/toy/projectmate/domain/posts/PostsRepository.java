7658df6eb49e2d4b52c601eba677672ec3b04395
Young <92440521+srp0114@users.noreply.github.com>
2022-12-22 18:54:41 +0900

    Delete Server

--- server/projectmate/src/main/java/com/toy/projectmate/domain/posts/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.toy.projectmate.domain.posts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-}
