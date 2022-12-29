1fb476e58aa1b7893998296848ab3db99d50dc81
srp0114 <ksy010114@gmail.com>
2022-12-30 01:48:26 +0900

    API 데이터 읽어오기

--- client/projectmate/src/components/pages/Details.js
+++ client/projectmate/src/components/pages/Details.js
@@ -1,58 +1,89 @@
-import '../App.css';
-import React from 'react';
+import './components/css/Details.css';
+import {React, useState} from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Divider, Space, Typography, Input } from 'antd';
+import axios from 'axios';
 
 const { Title, Text } = Typography;
 const { TextArea } = Input;
 
 function App() {
+  const [posting, setPosting] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [content ,setContent] = useState([]);
+  const [writer, setWriter] = useState([]);
+  const [subject, setSubject] = useState([]);
+  const [division, setDivision] = useState([]);
+  const [peopleNum, setPeopleNum] = useState([]);
+  const [proceedWay, setProceedWay] = useState([]);
+  const [isProgress, setIsProgress] = useState([]);
+  const [date, setDate] = useState([]);
+  var config = {
+    method: 'get',
+    url: 'http://localhost:8080/post/4',
+    headers: { }
+  };
+
+  axios(config)
+  .then(function (response) {
+    setPosting(JSON.stringify(response.data));
+    setTitle(JSON.stringify(response.data.title));
+    setContent(JSON.stringify(response.data.content));
+    setWriter(JSON.stringify(response.data.writer));
+    setSubject(JSON.stringify(response.data.subject));
+    setDivision(JSON.stringify(response.data.division));
+    setPeopleNum(JSON.stringify(response.data.people_num));
+    setProceedWay(JSON.stringify(response.data.proceed_way));
+    setIsProgress(JSON.stringify(response.data.is_progress));
+    setDate(JSON.stringify(response.data.modifiedDate));
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  }); 
+
+
   return (
-    <div className="App">
-      <p className="postingDay">2022.12.18 20:43</p>
-
-      <Title level={1} className="postingTitle">팀원 구합니다!</Title>
-
+    <>
+    <div className="posting">
+      <p className="postingDay">{date}</p>
+      <Title level={1} className="postingTitle">{title}</Title>
       <br/>
       <Space align="center">
-        <Avatar size={38} icon={<UserOutlined/>}/> 
-        <Text fontSize={100}>종강원해 (20)</Text> 
+        <Avatar size={38} icon={<UserOutlined/>}/>
+        <Text fontSize={100}>{writer}</Text>
       </Space>
-
       <Divider/>
-
       <div className="postingInfo">
-      <Space align="center" size={235}>
-        <Space align="center" size={100}> 
-        <Title level={4}>과목명</Title>
-        <Title level={4}>웹프레임워크1</Title>       
+        <Space align="center" size={235}>
+          <Space align="center" size={100}>
+            <Title level={4}>과목명</Title>
+            <Title level={4}>{subject}</Title>
+          </Space>
+          <Space align="center" size={135}>
+            <Title level={4}>분반</Title>
+            <Title level={4}>{division}</Title>
+          </Space>
         </Space>
-      
-        <Space align="center" size={135}>
-        <Title level={4}>분반</Title>
-        <Title level={4}>B반</Title>
+        <Space align="center" size={315}>
+          <Space align="center" size={85}>
+            <Title level={4}>모집인원</Title>
+            <Title level={4}>{peopleNum}</Title>
+          </Space>
+          <Space align="center" size={100}>
+            <Title level={4}>진행방식</Title>
+            <Title level={4}>{proceedWay}</Title>
+          </Space>
         </Space>
-      </Space>
-
-      <Space align="center" size={315}>
-        <Space align="center" size={85}> 
-        <Title level={4}>모집인원</Title>
-        <Title level={4}>4명</Title>
-        </Space>
-
-        <Space align="center" size={100}>
-        <Title level={4}>진행방식</Title>
-        <Title level={4}>오프라인</Title>
-        </Space>
-      </Space>
       </div>
       <Divider style={{ borderWidth: 5, borderColor: 'grey' }}  />
       <br/>
       <br/>
       <TextArea readOnly={true} autoSize={{ minRows: 2, maxRows: 6 }} style={{resize: 'none', border:'none', fontSize:'18px'}}
-      value="팀플 주제는 아직 미정이지만, 쇼핑몰 프로젝트로 진행해볼까합니다. 
-      깃을 조금이라도 써보신 분이면 좋을 것 같고 연락 잘 되고, 열정적으로 참여하실 분 찾고 있습니다!"/>
+      value={content}/>
     </div>
+    <p>{posting}</p>
+    </>
   );
 }
 
