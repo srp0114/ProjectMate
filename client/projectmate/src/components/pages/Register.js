573dc9fcdcfc75d33641c83aa51f2871891148cc
srp0114 <ksy010114@gmail.com>
2022-12-31 16:57:40 +0900

    toast-ui 삭제

--- client/projectmate/src/components/pages/Register.js
+++ client/projectmate/src/components/pages/Register.js
@@ -1,5 +1,3 @@
-import { Editor } from '@toast-ui/react-editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
 import { Divider, Typography } from 'antd';
 import "./components/css/Details.css"
 
@@ -12,20 +10,7 @@ function App() {
 
       <Divider/>
       <Title level={2}>프로젝트를 소개해주세요</Title>
-      <Editor
-      initialValue="hello react editor world!"
-      previewStyle="vertical"
-      height="600px"
-      initialEditType="wysiwyg"
-      useCommandShortcut={false}
-      toolbarItems={[
-        ['heading', 'bold', 'italic', 'strike'],
-        ['hr', 'quote'],
-        ['ul', 'ol', 'task', 'indent', 'outdent'],
-        ['table', 'image', 'link'],
-        ['code', 'codeblock']
-      ]}
-      />
+      
     </div>
   );
 }
