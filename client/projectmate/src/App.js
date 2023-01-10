d6d9084237f503717806cbdc87482b8af1bc84a6
srp0114 <ksy010114@gmail.com>
2023-01-11 04:43:32 +0900

    상세보기 넘어가기 구현

--- client/projectmate/src/App.js
+++ client/projectmate/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import './style.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header'
 import Home from './components/Home'
 import Banner from './components/Banner'
 import LoginHeader from './components/LoginHeader'
+import Details from "./components/pages/Details"
+import React from 'react';
+
 function App() {
-  return (
-   <>
-    <div className='home'>
-      <Home/>
-    </div>
-   </>
-  );
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/post/:id" element={<Details/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
