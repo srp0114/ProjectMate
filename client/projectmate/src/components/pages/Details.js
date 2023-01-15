0e22f83e9249bd07605228aaac00f53eddac35f1
wanho <bwh0607@gmail.com>
2023-01-15 17:35:52 +0900

    복구
    복구

--- client/projectmate/src/components/pages/Details.js
+++ client/projectmate/src/components/pages/Details.js
@@ -1,15 +1,14 @@
-import '../css/Details.css'
-import {React, useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Divider, Space, Typography, Input, Button } from 'antd';
 import axios from 'axios';
 import Comments from './Comments';
-import { useLinkClickHandler } from 'react-router-dom';
-
+import Edit from "./Edit.js"
+import { useLinkClickHandler, useParams, useNavigate } from 'react-router-dom';
+import '../css/Details.css'
 const { Title, Text } = Typography;
 const { TextArea } = Input;
-
-const Details=()=> {
+const Details = () => {
   const [posting, setPosting] = useState([]);
   const [title, setTitle] = useState([]);
   const [content ,setContent] = useState([]);
@@ -22,27 +21,35 @@ const Details=()=> {
   const [date, setDate] = useState([]);
   const [postId, setPostId]= useState();
   const [commentList, setCommentList] = useState([]);
-  
-  const auth = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxODkxMTk4Iiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY3MzU3MjA2MCwiZXhwIjoxNjczNTc1NjYwfQ.9kXYhr08b2V2fjjQg4t7OTiE6Pmy9wzTZJz1b_gJvOw';
-  const method = 'get';
-  
-  var config = {
-    method: `${method}`,
-    url: '/post/1',
+  const [isWriter, setIsWriter] = useState([]);
+  const {id} = useParams();
+  const auth = localStorage.getItem("token")
+  const goToHome = useNavigate();
+  const goToUpload = useNavigate();
+
+  var getConfig = {
+    method: 'get',
+    url: `/post/${id}`,
     headers: { 
-      'Authorization': `${auth}`,
-      'Content-Type': 'application/json'
-    },
+      'Authorization': `Bearer ${auth}`
+    }
   };
 
+  var deleteConfig = {
+    method: 'delete',
+    url: `/post/${id}`,
+    headers: { 
+      'Authorization': `Bearer ${auth}`
+    }
+  }
   useEffect (() => {
-    axios(config)
+    axios(getConfig)
 	  .then(function(response) {
-      console.log('가져오기성공')
+      console.log('가져오기 성공')
 	    setPosting(JSON.stringify(response.data));
 	    setTitle(response.data.title);
 	    setContent(response.data.content);
-	    setWriter(response.data.writer);
+	    setWriter(response.data.writer_nickname);
 	    setSubject(response.data.subject);
 	    setDivision(response.data.division);
 	    setPeopleNum(response.data.people_num);
@@ -51,21 +58,32 @@ const Details=()=> {
 	    setDate(response.data.modifiedDate);
       setCommentList(response.data.commentList);
       setPostId(response.data.id);
+      setIsWriter(response.data.isWriter);
 	  })
 	  .catch(function (error) {
 	    console.log(error);
 	  }); 
   }, [])
-
-  const DeletePosting = () => {
-    method=`delete`;
-    axios(config)
-        .then(response => console.log('게시글 삭제 성공'))
+  const EditPost = () => {
+    goToUpload(`/edit/${postId}`)
+  }
+  const EditButton = isWriter ? (
+    <Button onClick={EditPost}>수정하기</Button>
+  ) : (null);
+  
+  const DeletePost = () => {
+    axios(deleteConfig)
+        .then(response => {
+          console.log('게시글 삭제 성공')
+          goToHome('/')
+        })
         .catch(error => {
             console.error(error);
-    });
+        });
   }
-
+  const DeleteButton = isWriter ? (
+    <Button onClick={DeletePost}>삭제하기</Button>
+  ) : (null);
   return (
     <>
     <div className="posting">
@@ -75,45 +93,46 @@ const Details=()=> {
       <Space align="center">
         <Avatar size={38} icon={<UserOutlined/>}/>
         <Text fontSize={100}>{writer}</Text>
+        {EditButton}
+        {DeleteButton}
       </Space>
       <Divider/>
       <div className="postingInfo">
-        <Space align="center" size={300}>
-          <Space align="center" size={100}>
-            <Title level={4}>과목명</Title>
-            <Title level={4}>{subject}</Title>
-          </Space>
-          <Space align="center" size={135}>
-            <Title level={4}>분반</Title>
-            <Title level={4}>{division}</Title>
-          </Space>
+      <Space align="center" size={300}>
+        <Space align="center" size={100}>
+          <Title level={4}>과목명</Title>
+          <Title level={4}>{subject}</Title>
         </Space>
-        <Space align="center" size={357}>
-          <Space align="center" size={85}>
-            <Title level={4}>모집인원</Title>
-            <Title level={4}>{peopleNum}</Title>
-          </Space>
-          <Space align="center" size={100}>
-            <Title level={4}>진행방식</Title>
-            <Title level={4}>{proceedWay}</Title>
-          </Space>
+        <Space align="center" size={135}>
+          <Title level={4}>분반</Title>
+          <Title level={4}>{division}</Title>
         </Space>
+      </Space>
+      <Space align="center" size={357}>
+        <Space align="center" size={85}>
+          <Title level={4}>모집인원</Title>
+          <Title level={4}>{peopleNum}</Title>
+        </Space>
+        <Space align="center" size={100}>
+          <Title level={4}>진행방식</Title>
+          <Title level={4}>{proceedWay}</Title>
+        </Space>
+      </Space>
       </div>
       <Divider style={{ borderWidth: 5, borderColor: 'grey' }}  />
       <br/>
       <br/>
-      <TextArea readOnly={true} autoSize={{ minRows: 2, maxRows: 6 }} 
-                style={{resize: 'none', border:'none', fontSize:'18px'}} value={content}/>
+      {/* <TextArea readOnly={true} autoSize={{ minRows: 2, maxRows: 6 }} 
+                style={{resize: 'none', border:'none', fontSize:'18px'}} value={content}/> */}
+      <div dangerouslySetInnerHTML = { {  __html : content } }></div>
       <br/>
       <Divider/>
       <Title level={2} className="postingTitle">댓글</Title>
       <div>
-        <Comments commentList={commentList} auth = {auth}/>
+      <Comments commentList={commentList} auth = {auth}/>
       </div>
-      <Button onClick={DeletePosting}>삭제하기</Button>
     </div>
     </>
   );
 }
-
 export default Details;
